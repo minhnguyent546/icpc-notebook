30b4cd3d77151e96a795f4f7983eea4be193128b
minhnguyent546 <minhnguyent546@gmail.com>
2022-10-29 16:04:43 +0700

    add README.md and .gitignore

---
+++ source/string/finding_borders.h
@@ -0,0 +1,18 @@
+/**
+ * Description: given a string s of length n, a prefix of s is called a border of s if it is also a suffix of s.
+ * Tested: https://cses.fi/problemset/task/1732
+ */
+vector<int> find_borders(const string &s) {
+    int n = (int) s.size();
+    vector<int> pi = prefix_function(s);
+    vector<int> lengths;
+    int i = n - 1;
+    while (i >= 0) {
+        if (pi[i] != 0) {
+            lengths.emplace_back(pi[i]);
+        }
+        i = pi[i] - 1;
+    }
+    reverse(lengths.begin(), lengths.end());
+    return lengths;
+}
