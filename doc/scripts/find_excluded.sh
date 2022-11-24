dffd000b4576fd1e80f0fa7c48aa4a1f54637a82
minhnguyent546 <minhnguyent546@gmail.com>
2022-11-24 08:01:56 +0700

    includecpp => negzeroinclude and add doc

---
+++ doc/scripts/find_excluded.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p build/
+grep -PRho '^\s*\\negzeroinclude(\[\w*\])?{\K.*' ../../src/ | sed 's/.$//' > build/included_headers
+extensions=(h py java bashrc sublime-build)
+for extension in "${extensions[@]}"; do 
+	find ../../src/ -name "*.$extension" | grep -vFf ./build/included_headers
+done
--- src/number-theory/pollard_rho.h
+++ src/number-theory/pollard_rho.h
@@ -1,48 +1,44 @@
+using num_t = long long;
 const int PRIME_MAX = (int) 4e4; // for handle numbers <= 1e9.
 const int LIMIT = (int) 1e9;
 vector<int> primes;
-
+int small_primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 73, 113, 193, 311, 313, 407521, 299210837};
 void linear_sieve(int n);
-num_type mulmod(num_type a, num_type b, num_type mod);
-num_type powmod(num_type a, num_type n, num_type mod);
-
-bool miller_rabin(num_type a, num_type d, int s, num_type mod) {
-    // mod - 1 = a ^ (d * 2^s).
-    num_type x = powmod(a, d, mod);
-    if (x == 1 || x == mod - 1) return true;
-    for (int i = 1; i <= s - 1; ++i) {
+num_t mulmod(num_t a, num_t b, num_t mod);
+num_t powmod(num_t a, num_t n, num_t mod);
+bool miller_rabin(num_t a, num_t d, int s, num_t mod) {
+    num_t x = powmod(a, d, mod);
+    if (x == mod - 1 || x == 1) {
+        return true;
+    }
+    for (int i = 0; i < s - 1; ++i) {
         x = mulmod(x, x, mod);
         if (x == mod - 1) return true;
     }
     return false;
 }
-bool is_prime(num_type n, int ITERATION = 10) {
-    if (n < 4) return (n == 2 || n == 3);
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    num_type d = n - 1;
+bool is_prime(num_t n, int tests = 10) {
+    if (n < 4) return (n > 1);
+    num_t d = n - 1;
     int s = 0;
-    while (d % 2 == 0) {
-        d /= 2;
-        s++;
-    }
-    for (int i = 0; i < ITERATION; ++i) {
-        num_type a = (num_type) (rand() % (n - 2)) + 2;
-        if (miller_rabin(a, d, s, n) == false) {
-            return false;
-        }
+    while (d % 2 == 0) { d >>= 1; s++; }
+    for (int i = 0; i < tests; ++i) {
+        int a = small_primes[i];
+        if (n == a) return true;
+        if (n % a == 0 || !miller_rabin(a, d, s, n)) return false;
     }
     return true;
 }
-num_type f(num_type x, int c, num_type mod) { // f(x) = (x^2 + c) % mod.
+num_t f(num_t x, int c, num_t mod) { // f(x) = (x^2 + c) % mod.
     x = mulmod(x, x, mod);
     x += c;
     if (x >= mod) x -= mod;
     return x;
 }
-num_type pollard_rho(num_type n, int c) {
+num_t pollard_rho(num_t n, int c) {
     // algorithm to find a random divisor of `n`.
     // using random function: f(x) = (x^2 + c) % n.
-    num_type x = 2, y = x, d;
+    num_t x = 2, y = x, d;
     long long p = 1;
     int dist = 0;
     while (true) {
@@ -54,30 +50,30 @@ num_type pollard_rho(num_type n, int c) {
     }
     return d;
 }
-void factorize(int n, vector<num_type> &factors);
-void llfactorize(num_type n, vector<num_type> &factors) {
+void factorize(int n, vector<num_t> &factors); 
+void llfactorize(num_t n, vector<num_t> &factors) {
     if (n < 2) return;
-    if (n < LIMIT) {
-        factorize(n, factors);
-        return;
-    }
     if (is_prime(n)) {
         factors.emplace_back(n);
         return;
     }
-    num_type d = n;
+    if (n < LIMIT) {
+        factorize(n, factors);
+        return;
+    }
+    num_t d = n;
     for (int c = 2; d == n; c++) {
         d = pollard_rho(n, c);
     }
     llfactorize(d, factors);
     llfactorize(n / d, factors);
 }
-vector<num_type> gen_divisors(vector<pair<num_type, int>> &factors) {
-    vector<num_type> divisors = {1};
+vector<num_t> gen_divisors(vector<pair<num_t, int>> &factors) {
+    vector<num_t> divisors = {1};
     for (auto &x : factors) {
         int sz = (int) divisors.size();
         for (int i = 0; i < sz; ++i) {
-            num_type cur = divisors[i];
+            num_t cur = divisors[i];
             for (int j = 0; j < x.second; ++j) {
                 cur *= x.first;
                 divisors.push_back(cur);
