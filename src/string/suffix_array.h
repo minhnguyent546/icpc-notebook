c5d0004afb0e84a72f41cd160d89ea189eb23594
minhnguyent546 <minhnguyent546@gmail.com>
2023-09-22 13:51:42 +0700

    update suffix array

--- src/string/suffix_array.h
+++ src/string/suffix_array.h
@@ -1,14 +1,23 @@
+/**
+ * Description: suffix array is a sorted array of all the suffixes of a given string.
+ * Usage:
+ *   sa[i] = starting index of the i-th smallest suffix.
+ *   rank[i] = rank of the suffix starting at `i`.
+ *   lcp[i] = longest common prefix between `sa[i - 1]` and `sa[i]`
+ *   for arbitrary `u v`, let i = rank[u] - 1, j = rank[v] - 1 (assume i < j), then:
+ *     longest_common_prefix(u, v) = min(lcp[i + 1], lcp[i + 2], ..., lcp[j])
+ * Time: O(NlogN).
+ */
 struct SuffixArray {
     string s;
     int n, lim;
     vector<int> sa, lcp, rank;
-    SuffixArray(const string &_s, int _lim = 256) : s(_s), n(s.length() + 1), lim(_lim), sa(n), lcp(n), rank(n) {
+    SuffixArray(const string &_s, int _lim = 256) : s(_s), n(s.length() + 1),
+        lim(_lim), sa(n), lcp(n), rank(n) {
         s += '$';
-        build();
-        kasai();
-        sa.erase(sa.begin());
-        lcp.erase(lcp.begin());
-        s.pop_back();
+        build(); kasai();
+        sa.erase(sa.begin()); lcp.erase(lcp.begin());
+        rank.pop_back(); s.pop_back();
     }
     void build() {
         vector<int> nrank(n), norder(n), cnt(max(n, lim));
@@ -16,17 +25,16 @@ struct SuffixArray {
             sa[i] = i; rank[i] = s[i];
         }
         for (int k = 0, rank_cnt = 0; rank_cnt < n - 1; k = max(1, k * 2), lim = rank_cnt + 1) {
-            // counting sort. 
-            for (int i = 0; i < n; ++i) norder[i] = (sa[i] - k + n) % n;
-            for (int i = 0; i < n; ++i) cnt[rank[i]]++;
+            for (int i = 0; i < n; ++i) {
+                norder[i] = (sa[i] - k + n) % n;
+                cnt[rank[i]]++;
+            }
             for (int i = 1; i < lim; ++i) cnt[i] += cnt[i - 1];
             for (int i = n - 1; i >= 0; --i) sa[--cnt[rank[norder[i]]]] = norder[i];
             rank[sa[0]] = rank_cnt = 0;
             for (int i = 1; i < n; ++i) {
                 int u = sa[i], v = sa[i - 1];
-                int nu = u + k, nv = v + k;
-                if (nu >= n) nu -= n; 
-                if (nv >= n) nv -= n;
+                int nu = (u + k) % n, nv = (v + k) % n;
                 if (rank[u] != rank[v] || rank[nu] != rank[nv]) ++rank_cnt;
                 nrank[sa[i]] = rank_cnt;
             }
@@ -35,12 +43,10 @@ struct SuffixArray {
         }
     }
     void kasai() {
-        for (int i = 0; i < n; ++i) rank[sa[i]] = i;
         for (int i = 0, k = 0; i < n - 1; ++i, k = max(0, k - 1)) {
             int j = sa[rank[i] - 1];
             while (s[i + k] == s[j + k]) k++;
             lcp[rank[i]] = k;
         }
-        // Note: lcp[i] = longest common prefix(sa[i - 1], sa[i]).
     }
 };
--- src/string/suffix_array_slow.h
+++ src/string/suffix_array_slow.h
@@ -1,23 +1,41 @@
-vector<int> suffix_array_slow(string &s) {
-    s += '$';
-    int n = (int) s.size();
-    vector<int> order(n), rank(n);
-    for (int i = 0; i < n; ++i) {
-        order[i] = i; rank[i] = s[i];
+/**
+ * Description: an easier way to implement suffix array but run slower
+ * Time: O(N * logN^2)
+ */
+struct SuffixArraySlow {
+    string s;
+    int n;
+    vector<int> sa, lcp, rank;
+    SuffixArraySlow(const string &_s): s(_s), n((int) s.size() + 1), sa(n), lcp(n), rank(n) {
+        s += '$';
+        build(); kasai();
+        sa.erase(sa.begin()); lcp.erase(lcp.begin());
+        rank.pop_back(); s.pop_back();
     }
-    for (int k = 0; k < n; k = max(1, k * 2)) {
-        stable_sort(order.begin(), order.end(), [&](int i, int j) {
-            return make_pair(rank[i], rank[(i + k) % n]) < make_pair(rank[j], rank[(j + k) % n]);
-        });
+    bool comp(int i, int j, int k) {
+        return make_pair(rank[i], rank[(i + k) % n]) < make_pair(rank[j], rank[(j + k) % n]);
+    }
+    void build() {
         vector<int> nrank(n);
-        for (int i = 0, cnt = 0; i < n; ++i) {
-            if (i > 0 && rank[order[i]] != rank[order[i - 1]]) ++cnt;
-            else if (i > 0 && rank[(order[i] + k) % n] != rank[(order[i - 1] + k) % n]) ++cnt;
-            nrank[order[i]] = cnt;
+        for (int i = 0; i < n; ++i) {
+            sa[i] = i; rank[i] = s[i];
         }
-        rank.swap(nrank);
+        for (int k = 0; k < n; k = max(1, k * 2)) {
+            stable_sort(sa.begin(), sa.end(), [&](int i, int j) {
+                return comp(i, j, k);
+            });
+            for (int i = 0, cnt = 0; i < n; ++i) {
+                if (i > 0 && comp(sa[i - 1], sa[i], k)) ++cnt;
+                nrank[sa[i]] = cnt;
+            }
+            rank.swap(nrank);
+        }
     }
-    s.pop_back(); order.erase(order.begin());
-    return order;
-    // Time complexity: O(N * log(N)^2). 
-}
+    void kasai() {
+        for (int i = 0, k = 0; i < n - 1; ++i, k = max(0, k - 1)) {
+            int j = sa[rank[i] - 1];
+            while (s[i + k] == s[j + k]) ++k;
+            lcp[rank[i]] = k;
+        }
+    }
+};
