2d2471b2ac105565eb0a3a33f36633e56ba32ba4
minhnguyent546 <minhnguyent546@gmail.com>
2023-10-02 12:24:40 +0700

    update linear-algebra

---
+++ src/linear-algebra/bareiss_determinant.h
@@ -0,0 +1,35 @@
+/**
+ * Description: Bareiss algorithm for computing determinant of a square matrix A
+ *   with integer entries using only integer arithmetic.
+ * Time complexity: O(N^3).
+ * Usage:
+ *   - Kirchhoff's theorem: finding the number of spanning trees.
+ */
+long long determinant(vector<vector<long long>> A) {
+    int n = (int) A.size();
+    long long prev = 1;
+    int sign = 1;
+    for (int i = 0; i < n - 1; ++i) {
+        // find non-zero cell
+        if (A[i][i] == 0) {
+            int k = -1;
+            for (int j = i + 1; j < n; ++j) {
+                if (A[j][i] != 0) {
+                    k = i;
+                    break;
+                }
+            }
+            if (k == -1) return 0;
+            swap(A[i], A[k]);
+            sign = -sign;
+        }
+        for (int j = i + 1; j < n; ++j) {
+            for (int k = i + 1; k < n; ++k) {
+                assert((A[j][k] * A[i][i] - A[j][i] * A[i][k]) % prev == 0);
+                A[j][k] = (A[j][k] * A[i][i] - A[j][i] * A[i][k]) / prev;
+            }
+        }
+        prev = A[i][i];
+    }
+    return sign * A[n - 1][n - 1];
+}
---
+++ src/linear-algebra/gauss_determinant.h
@@ -0,0 +1,38 @@
+/**
+ * Description: computing determinant of a square matrix A by applying
+ *   Gauss elimination to produces a row echolon matrix B, then the
+ *   determinant of A is equal to product of the elements of the diagonal of B.
+ * Time complexity: O(N^3).
+ */
+const double EPS = 1e-9;
+double determinant(vector<vector<double>> A) {
+    int n = (int) A.size();
+    double det = 1;
+    for (int i = 0; i < n; ++i) {
+        // find non-zero cell
+        int k = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (abs(A[j][i]) > abs(A[k][i])) k = j;
+        }
+        if (abs(A[k][i]) < EPS) {
+            det = 0;
+            break;
+        }
+        if (i != k) {
+            swap(A[i], A[k]);
+            det = -det;
+        }
+        det *= A[i][i];
+        for (int j = i + 1; j < n; ++j) {
+            A[i][j] /= A[i][i];
+        }
+        for (int j = 0; j < n; ++j) {
+            if (j != i && abs(A[j][i]) > EPS) {
+                for (int k = i + 1; k < n; ++k) {
+                    A[j][k] -= A[i][k] * A[j][i];
+                }
+            }
+        }
+    }
+    return det;
+}
