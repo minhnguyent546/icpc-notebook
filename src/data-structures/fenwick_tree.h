28202fdae357e63f99b6daf9e85e688394d7ad67
minhnguyent546 <minhnguyent546@gmail.com>
2024-09-08 15:38:58 +0700

    feat: update data_structures/fenwick_tree.h
    * Simplify the implementations
    * Create better descripion and add formula for constructing a 2D Fenwick tree

--- src/data-structures/fenwick_tree.h
+++ src/data-structures/fenwick_tree.h
@@ -1,62 +1,32 @@
 /**
- * Description: range update and range sum query.
+ * Description: a minimal and simple data structure for point update and range query
+ * Note:
+ * - For range update and point query, create a Fenwick tree on array $D$ defined by $D_0 = A_0, D_i = A_i - A_{i - 1}$
+ * - For range update and range query, the idea is the same as above, but we can calculate the prefix as follow: $\sum\limits_{i = 0}^{k}{A_i} = \sum\limits_{i = 0}^{k}{\sum\limits_{j = 0}^{i}{D_j}} = (k + 1)\sum\limits_{i = 0}^{k}{D_i} - \sum\limits_{i = 0}^{k}{iD_i}$, thus we can maintain two prefix sums, $D_i$ and $iD_i$, with two Fenwick trees.
+ * Time: $O(\log{N})$
  */
 
-using tree_type = long long;
-struct FenwickTree {
+template<typename T>
+struct Fenwick {
     int n;
-    vector<tree_type> fenw_coeff, fenw;
-    FenwickTree() {}
-    FenwickTree(int _n) : n(_n) {
-        fenw_coeff.assign(n, 0); // fenwick tree with coefficient (n - i).
-        fenw.assign(n, 0); // normal fenwick tree.
-    }
-    template<typename G>
-    void build(const vector<G> &A) {
-        assert((int) A.size() == n);
-        vector<int> diff(n);
-        diff[0] = A[0];
-        for (int i = 1; i < n; ++i) {
-            diff[i] = A[i] - A[i - 1];
-        }
-        fenw_coeff[0] = (long long) diff[0] * n;
-        fenw[0] = diff[0];
-        for (int i = 1; i < n; ++i) {
-            fenw_coeff[i] = fenw_coeff[i - 1] + (long long) diff[i] * (n - i);
-            fenw[i] = fenw[i - 1] + diff[i];
-        }
-        for (int i = n - 1; i >= 0; --i) {
-            int j = (i & (i + 1)) - 1;
-            if (j >= 0) {
-                fenw_coeff[i] -= fenw_coeff[j];
-                fenw[i] -= fenw[j];
-            }
-        }
-    }
-    void add(vector<tree_type> &fenw, int i, tree_type val) {
+    vector<T> tree
+    Fenwick() {}
+    Fenwick(int _n): n(_n), tree(n) {}
+    void add(int i, T val) {
         while (i < n) {
-            fenw[i] += val;
+            tree[i] += val;
             i |= (i + 1);
         }
     }
-    tree_type __prefix_sum(vector<tree_type> &fenw, int i) {
-        tree_type res{};
+    T pref(int i) {
+        T res{};
         while (i >= 0) {
-            res += fenw[i];
+            res += tree[i];
             i = (i & (i + 1)) - 1;
         }
         return res;
     }
-    tree_type prefix_sum(int i) {
-        return __prefix_sum(fenw_coeff, i) - __prefix_sum(fenw, i) * (n - i - 1);
-    }
-    void range_add(int l, int r, tree_type val) {
-        add(fenw_coeff, l, (n - l) * val);
-        add(fenw_coeff, r + 1, (n - r - 1) * (-val));
-        add(fenw, l, val);
-        add(fenw, r + 1, -val);
-    }
-    tree_type range_sum(int l, int r) {
-        return prefix_sum(r) - prefix_sum(l - 1);
+    T query(int l, int r) {
+        return pref(r) - pref(l - 1);
     }
 };
