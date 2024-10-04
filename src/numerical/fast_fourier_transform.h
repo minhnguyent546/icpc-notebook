736daaf08b89e864c1c74991a802580640658202
minhnguyent546 <minhnguyent546@gmail.com>
2024-10-05 00:34:01 +0700

    chore: change int to int64_t in numerical/fast_fourier_transform.h

--- src/numerical/fast_fourier_transform.h
+++ src/numerical/fast_fourier_transform.h
@@ -6,18 +6,18 @@
 const double PI = acos(-1);
 using Comp = complex<double>;
 int reverse_bit(int n, int lg) {
-    int res = 0;
+    int ret = 0;
     for (int i = 0; i < lg; ++i) {
-        if (n & (1 << i)) {
-            res |= (1 << (lg - i - 1));
+        if ((n >> i) & 1) {
+            ret |= (1 << (lg - i - 1));
         }
     }
-    return res;
+    return ret;
 }
 void fft(vector<Comp> &a, bool invert = false) {
     int n = (int) a.size();
     int lg = 0;
-    while (1 << (lg) < n) ++lg;
+    while ((1 << lg) < n) ++lg;
     for (int i = 0; i < n; ++i) {
         int rev_i = reverse_bit(i, lg);
         if (i < rev_i) swap(a[i], a[rev_i]);
@@ -37,9 +37,10 @@ void fft(vector<Comp> &a, bool invert = false) {
     }
     if (invert) for (int i = 0; i < n; ++i) a[i] /= n;
 }
-vector<int> mult(vector<int> &a, vector<int> &b) {
+template<typename T, typename G>
+vector<int64_t> mult(const vector<T> &a, const vector<G> &b) {
     vector<Comp> A(a.begin(), a.end()), B(b.begin(), b.end());
-    int n = (int) a.size(), m = (int) b.size(), p = 1;
+    int n = a.size(), m = b.size(), p = 1;
     while (p < n + m) p *= 2;
     A.resize(p), B.resize(p);
     fft(A, false);
@@ -48,9 +49,9 @@ vector<int> mult(vector<int> &a, vector<int> &b) {
         A[i] *= B[i];
     }
     fft(A, true);
-    vector<int> res(n + m - 1);
+    vector<int64_t> res(n + m - 1);
     for (int i = 0; i < n + m - 1; ++i) {
-        res[i] = (int) round(A[i].real());
+        res[i] = (int64_t) round(A[i].real());
     }
     return res;
 }
